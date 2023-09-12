7a1d8659f469e68ca57fd4eca266bc86322e8945
Simon Reiser <ein.simonreiser@gmail.com>
2023-09-12 21:27:40 +0900

    complete exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const logger = require('./logger');
+
 const dummy = (blogs) => {
     return 1;
 }
@@ -7,7 +9,21 @@ const totalLikes = (blogs) => {
         .reduce((sum,likes) => sum + likes,0);
 }
 
+/**
+ * @param blogs
+ * @returns (1st) blog with highest likes of given blogs array, null if an empty array is given
+ */
+const favoriteBlog = (blogs) => {
+    if(blogs.length===0){
+        return null;
+    }
+
+    const maxLikes = Math.max(...blogs.map((blog) => blog.likes));
+    return blogs.find((blog) => blog.likes===maxLikes);
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
