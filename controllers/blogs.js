7887106326e8739c18e8aada2cf2e8630104f917
Simon Reiser <ein.simonreiser@gmail.com>
2023-09-16 20:47:27 +0900

    complete exercise 4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,15 +4,6 @@ const Blog = require('../models/blog');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-
-const getTokenFrom = request => {
-    const authorization = request.get('authorization');
-    if(authorization && authorization.startsWith('Bearer ')){
-        return authorization.replace('Bearer ','');
-    }
-    return null;
-}
-
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user',{ username:1,name:1 });
     return response.json(blogs)
@@ -21,7 +12,7 @@ blogRouter.get('/', async (request, response) => {
 blogRouter.post('/', async (request, response) => {
     logger.info('creating blog entry for body',request.body);
 
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
     if(!decodedToken.id) {
         return response.status(401).json({ error:'invalid token' });
     }
